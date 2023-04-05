435f3452409c2829e5c91b69a41cfd8eab685038
JurgenKs <jurgen.k8@hotmail.nl>
2023-04-05 22:55:35 +0200

    NotificationService Initialized with RabbitMQ messaging

---
+++ NotificationService.Core/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using OFOS.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.Core
+{
+    public interface INotificationService
+    {
+        void ConsumeEmailMessage(string emailMessage);
+    }
+}
